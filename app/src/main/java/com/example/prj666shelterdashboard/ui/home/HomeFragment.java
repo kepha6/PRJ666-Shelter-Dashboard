a0761983605747c71357fa7ba4f5169aff7cba2a
kp <kpham6@myseneca.ca>
2023-10-26 17:24:58 -0400

    Bar Graph?

--- app/src/main/java/com/example/prj666shelterdashboard/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/prj666shelterdashboard/ui/home/HomeFragment.java
@@ -14,6 +14,10 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.prj666shelterdashboard.R;
 import com.example.prj666shelterdashboard.databinding.FragmentHomeBinding;
 import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.LegendEntry;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.BarData;
 import com.github.mikephil.charting.data.BarDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
@@ -47,14 +51,13 @@ public class HomeFragment extends Fragment {
 
 
         // initializing variable for bar chart.
-        barChart = getActivity().findViewById(R.id.idBarChart);
-
+        barChart = root.findViewById(R.id.idBarChart);
 
         // calling method to get bar entries.
         getBarEntries();
 
         // creating a new bar data set.
-        barDataSet = new BarDataSet(barEntriesArrayList, "Geeks for Geeks");
+        barDataSet = new BarDataSet(barEntriesArrayList, "Home Shelters");
 
         // creating a new bar data and
         // passing our bar data set.
@@ -65,7 +68,7 @@ public class HomeFragment extends Fragment {
         barChart.setData(barData);
 
         // adding color to our bar data set.
-        barDataSet.setColors(ColorTemplate.MATERIAL_COLORS);
+        barDataSet.setColors(new int[]{Color.RED,Color.BLUE,Color.YELLOW,Color.GREEN});
 
         // setting text color.
         barDataSet.setValueTextColor(Color.BLACK);
@@ -73,6 +76,25 @@ public class HomeFragment extends Fragment {
         // setting text size
         barDataSet.setValueTextSize(16f);
         barChart.getDescription().setEnabled(false);
+        XAxis xAxis = barChart.getXAxis();
+        YAxis leftAxis = barChart.getAxisLeft();
+        YAxis rightAxis = barChart.getAxisRight();
+
+
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawLabels(false);
+
+        Legend legend = barChart.getLegend();
+        LegendEntry l1 = new LegendEntry("Toronto",Legend.LegendForm.DEFAULT,10f,2f,null, Color.RED);
+        LegendEntry l2 = new LegendEntry("Brampton",Legend.LegendForm.DEFAULT,10f,2f,null, Color.BLUE);
+        LegendEntry l3 = new LegendEntry("Markham",Legend.LegendForm.DEFAULT,10f,2f,null, Color.YELLOW);
+        LegendEntry l4 = new LegendEntry("Scarborough",Legend.LegendForm.DEFAULT,10f,2f,null, Color.GREEN);
+        legend.setCustom(new LegendEntry[]{l1,l2,l3,l4});
+        legend.setVerticalAlignment(Legend.LegendVerticalAlignment.BOTTOM);
+        legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+        legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+        legend.setDrawInside(false);
+
 
         return root;
     }
@@ -90,11 +112,9 @@ public class HomeFragment extends Fragment {
 
         // adding new entry to our array list with bar
         // entry and passing x and y axis value to it.
-        barEntriesArrayList.add(new BarEntry(1f, 4));
-        barEntriesArrayList.add(new BarEntry(2f, 6));
-        barEntriesArrayList.add(new BarEntry(3f, 8));
-        barEntriesArrayList.add(new BarEntry(4f, 2));
-        barEntriesArrayList.add(new BarEntry(5f, 4));
-        barEntriesArrayList.add(new BarEntry(6f, 1));
+        barEntriesArrayList.add(new BarEntry(1, -4));
+        barEntriesArrayList.add(new BarEntry(2, 6));
+        barEntriesArrayList.add(new BarEntry(3, 8));
+        barEntriesArrayList.add(new BarEntry(4, 10));
     }
 }
